99fc26c0416882ba9128bdc6cdef66e8858acb6e
Lorenayannnnn <lorenayan@qq.com>
2023-04-05 15:44:02 -0700

    init vilt

---
+++ models/ViLTForEmeme.py
@@ -0,0 +1,17 @@
+
+import torch
+from torch import nn
+from transformers import ViltModel, AutoConfig
+
+
+class ViLTForMemeSentimentClassification(nn.Module):
+    def __init__(self, config: AutoConfig, model_name_or_path: str, model_kwargs: dict):
+        super(ViLTForMemeSentimentClassification, self).__init__()
+        self.vilt_model = ViltModel.from_pretrained(
+            model_name_or_path,
+            config=config,
+            **model_kwargs
+        )
+
+    def forward(self, inputs):
+        return self.vilt_model(**inputs)['pooler_output']
