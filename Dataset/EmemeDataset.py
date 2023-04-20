c8fe4a3ed8975e8c8dcfb47730f6a1493a849a9e
Flora <florajia@usc.edu>
2023-04-19 17:57:06 -0700

    update dataset

--- Dataset/EmemeDataset.py
+++ Dataset/EmemeDataset.py
@@ -1,7 +1,8 @@
 import json
 import os
 from torch.utils import data
-from transformers import ViltProcessor
+from transformers import ViltProcessor, AutoTokenizer
+
 import torch
 import numpy as np
 from PIL import Image
@@ -20,12 +21,21 @@ class EmemeDataset(data.Dataset):
         self.data_dir = data_dir
         self.json_file_path_list = json_file_path_list
         self.processor = processor
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "arpanghoshal/EmoRoBERTa",
+            cache_dir="ememe_emoroberta_cache/"
+        )
         self.cached_data_file = '{}_{}.pkl'.format(out_file_name, "dev" if split == "test" else "train")
         self.emotion_list = emotion_list
         self.emotion2label = {item: index for index, item in enumerate(emotion_list)}
+        self.emotion_times = {item: 0 for index, item in enumerate(emotion_list)}
         self.num_labels = len(emotion_list)
         print(f"cached_data_file in {self.cached_data_file}")
         print("num_labels: ", self.num_labels)
+        if split == 'train':
+            need = 3000
+        else:
+            need = 500
 
         if os.path.exists(self.cached_data_file):
             # Load cached raw_json_data
@@ -40,19 +50,38 @@ class EmemeDataset(data.Dataset):
                     for entry in j:
                         image_url_list = entry["photos"]
                         # print("image_url_list: ", image_url_list)
-                        for image_url in image_url_list:
-                            example = {
-                                'text': entry["tweet"],
-                                'image_url': image_url,
-                                'emotion': entry["emotion"],
-                                'label': self.emotion2label[entry["emotion"]]
-                            }
-                            self.data.append(example)
+                        image_url = image_url_list[0]
+                        # for image_url in image_url_list:
+                        try:
+                            image = Image.open(requests.get(image_url, stream=True).raw).convert('RGB')
+                        except Exception:
+                            emotion = entry["emotion"]
+                            print(f"Error when reading {image_url} with emotion {emotion}")
+                            continue
+                        example = {
+                            'text': entry["tweet"],
+                            'image_url': image_url,
+                            'emotion': entry["emotion"],
+                            'label': self.emotion2label[entry["emotion"]],
+                            'image': image
+                        }
+                        self.emotion_times[entry["emotion"]] += 1
+                        self.data.append(example)
+                        need -= 1
+                        if (need%10 == 0):
+                            print(need)
+                        if (need <= 0):
+                            break
+                    if (need <= 0):
+                        break
+                if (need <= 0):
+                    break
 
             pkl.dump(self.data, open(self.cached_data_file, 'wb'))
 
         self.n_examples = len(self.data)
         print("Loaded tweets {} dataset, with {} examples".format(self.split, len(self.data)))
+        json.dump(self.emotion_times, open('emotion_times.json', 'w'))
 
     def __len__(self):
         return len(self.data)
@@ -63,19 +92,23 @@ class EmemeDataset(data.Dataset):
         image_url = example['image_url']
         emotion = example['emotion']
         label = example['label']
+        image = example['image']
 
-        try:
-            image = Image.open(requests.get(image_url, stream=True).raw).convert('RGB')
-            encoding = self.processor(image, text, padding="max_length", truncation=True, return_tensors="pt")
-            # remove batch dimension
-            for k, v in encoding.items():
-                encoding[k] = v.squeeze()
-            # add labels
-            encoding['labels'] = torch.tensor(np.array(label))
+        encoding = self.processor(image, text, padding="max_length", truncation=True, return_tensors="pt")
+        # remove batch dimension
+        for k, v in encoding.items():
+            encoding['vilt_input'][k] = v.squeeze()
+        # add labels
+        encoding['labels'] = torch.tensor(np.array(label))
+        emoroberta_input = self.tokenizer(text, return_tensors="pt")
+        for k, v in emoroberta_input.items():
+            encoding['emoroberta_input'][k] = v.squeeze()
 
-            return encoding
-        except Exception:
-            print(f"Error when reading {image_url} with emotion {emotion}")
+        return encoding
+        # except Exception:
+        #     print(f"Error when reading {image_url} with emotion {emotion}")
+
+
 
 
 def build_ememe_dataloader(batch_size: int,
@@ -86,7 +119,9 @@ def build_ememe_dataloader(batch_size: int,
     shuffle = split == 'train'
     dataloader = torch.utils.data.DataLoader(build_ememe_dataset(batch_size, data_dir, split, emotion_classes),
                                              batch_size=batch_size,
-                                             shuffle=shuffle)
+                                             shuffle=shuffle,
+                                            #  collate_fn=lambda x: batch_collate(x)
+                                             )
     return dataloader
 
 
